d0fe769ee478ecff76c8154077b2540fa1113a2f
jrafa <dj.czosnek@gmail.com>
2017-11-14 14:49:16 +0100

    change Button to TextView with autoLink

--- app/src/main/java/com/jrafa/helloandroid/MainActivity.java
+++ app/src/main/java/com/jrafa/helloandroid/MainActivity.java
@@ -1,11 +1,7 @@
 package com.jrafa.helloandroid;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +11,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     *  This method go to url after button is pressed.
-     */
-    public void goToPage(View view) {
-        TextView page = (TextView) (findViewById(R.id.url));
-        String url = "http://" + page.getText().toString();
-        Intent browse = new Intent( Intent.ACTION_VIEW , Uri.parse(url) );
-        startActivity(browse);
-    }
 }
